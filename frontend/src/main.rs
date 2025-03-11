58ad31f58fe4f4a16f84af56ff7be7efab89cf99
Karl Rosengren <karl.rosengren.kr@gmail.com>
2025-03-11 23:18:47 +0100

    Start to yew. Cant host for home network because im in wsl

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,28 @@
+use web_sys::window;
+#[warn(non_camel_case_types)]
+
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+}
+
+#[function_component]
+fn App() -> Html {
+    let onclick = Callback::from(|_| {
+        if let Some(win) = window() {
+            win.location().set_href("https://qrbot.net/x-callback-url/scan?x-success=127.0.0.1:8080").unwrap();
+        }
+    });
+    html! {
+        <button {onclick}>{ "Scan" }</button>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
